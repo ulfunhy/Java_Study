3c2f9a6392d795e4f21aba87c1a4775d5abad9ff
LeeHyunJin <myfamily11141@gmail.com>
2023-04-27 00:00:52 +0900

    ArrayList 예제(수정)

--- Pro03Collection/src/coll02/ArrayListEx.java
+++ Pro03Collection/src/coll02/ArrayListEx.java
@@ -5,28 +5,34 @@ import java.util.Scanner;
 
 public class ArrayListEx {
 	public static void main(String[] args) {
-		Arrray<String> a = new ArrayList<String>();
+		// Arrray<String> a = new ArrayList<String>();
+		// ArrayList<String> a = new ArrayList<>();
+		var a = new ArrayList<String>();
 		// 문자열 데이터를 담는 ArrayList객체변수 a를 만들고, 생성자가
 		
+		// 키보드 입력 객체 생성
 		Scanner sc = new Scanner(System.in);
-		
+				
 		for (int i = 0; i < 4; i++) {
 			System.out.print("이름을 입력하시오>>");
 			String s = sc.next();	// 키보드로 문자열을 입력받음
 			a.add(s);
 		}
-		
+				
 		for (int i = 0; i < 4; i++) {
 			String name = a.get(i);	// 0부터 데이터 가져온후 name에 저장
 			System.out.print(name + " ");
 		}
-		
-		// 이름이 가장 긴 것을 출력
-		// 입력된 데이터 Mike jane Ashely Helen
-		int longestIndex = 0;
-		for(int j = 1; i < a.size(); i++) {
-			if(a.get(longestIndex).length())}
+				
+		// 각 요소 저장되어 있는 데이터 길이 값을 가지고
+		// 그 길이의 최대값에 해당되는 인덱스를 구하는 알고리즘
+		int longestindex = 0;
+		for (int i = 1; i < a.size(); i++) {
+			if(a.get(longestindex).length() < a.get(i).length()) {
+				longestindex = i;
+			}
 		}
-	
-	System.out.println("\n가장 긴 이름은 : " + a.get(longestindex));
+				
+		System.out.println("\n가장 긴 이름은 : " + a.get(longestindex));
+	}
 }
